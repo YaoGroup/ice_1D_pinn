91f6eab963dce1150ec65a29e745f140ea65472b
Yuno Iwasaki <60745379+yuno11@users.noreply.github.com>
2024-02-09 09:28:41 +0900

    Update __init__.py

--- pinn_clusters/__init__.py
+++ pinn_clusters/__init__.py
@@ -4,5 +4,5 @@ from ._model import create_mlp
 #from ._loss import SquareLoss, SquareLossRandom
 #from ._formulations import get_collocation_points, to_tensor
 from ._optimization import LBFGS, Adam
-from ._error_func import gamma_batch
+#from ._error_func import gamma_batch
 from ._data import (add_noise, random_sample)
